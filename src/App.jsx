77710569b126304db9a49a794e852166665dc84a
maslog <ronaldmaslog5@gmail.com>
2024-07-29 10:48:27 +0800

    Using GH-Pages

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Counter from "./assets/Components/Counter";
+
+function App() {
+  return (
+    <div className="App">
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/ronaldmaslog2024-2025.github.io",
+  plugins: [react()],
+})
