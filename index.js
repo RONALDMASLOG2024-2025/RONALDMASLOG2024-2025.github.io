575fe283503aa022c545472f716521c9c72b562a
Ronald P. Maslog <rmaslog_230000000654@uic.edu.ph>
2024-10-20 22:45:42 +0800

    With Dark Mode na sya
    Dark mode

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-function scrollToSection() {
-    const element = document.getElementById("card-contact");
-    element.scrollIntoView({ behavior: "smooth" });
+window.onload = function () {
+  const isDark = localStorage.getItem("darkmode") === "true";
+  if (isDark) {
+    document.body.classList.add("darkmode");
   }
-  
-  
+};
+
+function darkmode() {
+  const body = document.body;
+  const isDark = body.classList.toggle("darkmode");
+  localStorage.setItem("darkmode", isDark);
+}
+
+function scrollToSection() {
+  const element = document.getElementById("card-contact");
+  element.scrollIntoView({ behavior: "smooth" });
+}
